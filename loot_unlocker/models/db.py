e5a0c02868990d1587b67808df0ce5f452f974e9
blueloveth <blueloveth@foxmail.com>
2025-03-26 21:57:51 +0800

    up

--- loot_unlocker/models/db.py
+++ loot_unlocker/models/db.py
@@ -1,1 +1,34 @@
-from loot_unlocker.env import get_postgres_conn
+from datetime import datetime
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class Player(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    platform: str = Field(index=True)
+    nickname: str = Field(index=True)
+    is_admin: bool = Field(default=False)
+    access_token: str = Field()
+
+class Project(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    name: str = Field(index=True)
+    description: str = Field()
+
+class CloudSave(SQLModel, table=True):
+    project_id: str = Field(foreign_key="project.id")
+    player_id: str = Field(foreign_key="player.id")
+    data: str = Field()
+    version: str = Field()
+    created_at: datetime = Field(default=datetime.now)
+
+class CloudLog(SQLModel, table=True):
+    project_id: str = Field(foreign_key="project.id")
+    player_id: str = Field(foreign_key="player.id")
+    data: str = Field()
+    created_at: datetime = Field(default=datetime.now)
+
+class Ugc(SQLModel, table=True):
+    project_id: str = Field(foreign_key="project.id")
+    player_id: str = Field(foreign_key="player.id")
+    type: str = Field()
+    data: str = Field()
+    created_at: datetime = Field(default=datetime.now)
