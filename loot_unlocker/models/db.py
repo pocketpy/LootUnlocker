20c1e513d57b0dd153d31ab4209853624a9ab92f
blueloveth <blueloveth@foxmail.com>
2025-03-26 22:41:08 +0800

    up

--- loot_unlocker/models/db.py
+++ loot_unlocker/models/db.py
@@ -4,31 +4,86 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 class Player(SQLModel, table=True):
     id: str = Field(primary_key=True)
     platform: str = Field(index=True)
-    nickname: str = Field(index=True)
+    nickname: str = Field(index=True, max_length=32)
+    avatar: str = Field()
     is_admin: bool = Field(default=False)
     access_token: str = Field()
+
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
 
 class Project(SQLModel, table=True):
     id: str = Field(primary_key=True)
     name: str = Field(index=True)
     description: str = Field()
+    latest_version: int = Field()
+
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+
+class ProjectVersion(SQLModel, table=True):
+    version: int = Field(primary_key=True)
+    comment: str = Field(default="")
+    data: str = Field()
+
+    project_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
 
 class CloudSave(SQLModel, table=True):
-    project_id: str = Field(foreign_key="project.id")
-    player_id: str = Field(foreign_key="player.id")
+    id: str = Field(primary_key=True)
     data: str = Field()
-    version: str = Field()
-    created_at: datetime = Field(default=datetime.now)
+    index: int = Field()
+
+    version: int = Field()
+    project_id: str = Field()
+    player_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+
 
 class CloudLog(SQLModel, table=True):
-    project_id: str = Field(foreign_key="project.id")
-    player_id: str = Field(foreign_key="player.id")
-    data: str = Field()
-    created_at: datetime = Field(default=datetime.now)
-
-class Ugc(SQLModel, table=True):
-    project_id: str = Field(foreign_key="project.id")
-    player_id: str = Field(foreign_key="player.id")
+    id: int = Field(primary_key=True)
     type: str = Field()
     data: str = Field()
-    created_at: datetime = Field(default=datetime.now)
+
+    version: int = Field()
+    project_id: str = Field()
+    player_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+
+
+class Ugc(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    type: str = Field()
+    data: str = Field()
+
+    version: int = Field()
+    project_id: str = Field()
+    player_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+
+
+class Image(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    base64: str = Field()
+    base64_thumbnail: str = Field(default="")
+    project_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+
+
+class File(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    filename: str = Field()
+    size: int = Field()
+    project_id: str = Field()
+    player_id: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now)
+
+
+if __name__ == '__main__':
+    from sqlalchemy.schema import CreateTable
+    from sqlalchemy.dialects import postgresql
+    print(CreateTable(Player.__table__).compile(dialect=postgresql.dialect()))
